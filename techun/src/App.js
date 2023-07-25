41626bda3092211267d30c575c8e8da17b24b8a9
Divya123pathak <divyadi.pathak@gmail.com>
2023-07-25 15:22:23 +0530

    commit

---
+++ techun/src/App.js
@@ -0,0 +1,52 @@
+import React ,{useState} from 'react';
+ import Expenses  from './componenets/Expenses/Expenses';
+ import NewExpense from './componenets/NewExpense/NewExpense';
+
+ let DUMMY_EXPENSE=[
+   {
+      id:'e1',
+      title:'School fee',
+      amount:300,
+      date:new Date(2021,5,12)
+   },
+   {
+      id:'e2',
+      title:'Tution fee',
+      amount:350,
+      date:new Date(2001,6,12)
+   },
+   {
+      id:'e3',
+      title:'Section fee',
+      amount:390,
+      date:new Date(2005,5,12)
+   },
+   {
+      id:'e4',
+      title:'Class fee',
+      amount:456,
+      date:new Date(2021,5,3)
+   }
+
+];
+
+const App =() => {
+   const [expenses,setExpenses]= useState(DUMMY_EXPENSE);
+   const addExpenseHandler = (expense) => {
+   const updatedExpense =[expense, ...expenses];
+   setExpenses( updatedExpense);
+};
+   
+    return (
+      <div>
+        <NewExpense onAddExpense={addExpenseHandler} />
+         
+        
+        <Expenses item={expenses} />
+      </div>
+     
+
+
+    );
+}
+export default App;
